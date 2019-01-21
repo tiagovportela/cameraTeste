7044c1b99682ecbfdaa05b9f5d68e935686b2f5a
Tiago Portela <tiagovportela@gmail.com>
2019-01-21 17:29:24 +0000

    Add files via upload

---
+++ FlaskTutorial-master/main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/cam")
+def cam():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True, threaded=True)
+
+
